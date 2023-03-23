3afb97bee6333b253f0565b2102a74178cbfa600
EricTomaszewski <ireneusz.tomaszewski@gmail.com>
2023-03-23 23:41:31 +0000

    P8 - Number 1 to 100 all under each other AJAX

--- Flask/0008-Numbers1to100AJAX/0008-Numbers1to100AJAX.py
+++ Flask/0008-Numbers1to100AJAX/0008-Numbers1to100AJAX.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, Response
+from flask import Flask, jsonify, render_template
+import threading
 import time
 
 app = Flask(__name__)
 
+numbers = []
+
+def update_numbers():
+    global numbers
+    for i in range(1, 101):
+        numbers.append(i)
+        time.sleep(1)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', numbers=numbers)
 
-def generate():
-    yield '1\n'
-    time.sleep(5)
-    yield '2\n'
-    time.sleep(5)
-    yield '100\n'
-
-@app.route('/numbers')
-def numbers():
-    return Response(generate(), mimetype='text/plain')
+@app.route('/get_numbers')
+def get_numbers():
+    return jsonify({'numbers': numbers})
 
 if __name__ == '__main__':
+    import threading
+    t = threading.Thread(target=update_numbers)
+    t.start()
     app.run(debug=True)
