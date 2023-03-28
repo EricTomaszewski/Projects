c19ee5dc3ef3967232619b3f9ee2f73bb3473c36
EricTomaszewski <ireneusz.tomaszewski@gmail.com>
2023-03-29 00:05:39 +0100

    P16 - cleaning the code from previous versions

--- Flask/0016-LinksFromScriptAJAX/0016-LinksFromScriptAJAX.py
+++ Flask/0016-LinksFromScriptAJAX/0016-LinksFromScriptAJAX.py
@@ -9,12 +9,8 @@ links = []
 def index():
     return render_template('index.html', links=links)
 
-'''@app.route('/get_link')
-def get_links():
-    link = next(links_spit())
-    links.append(link)
-    return jsonify({'link': link})'''
 
+### prints a single letter per line
 '''@app.route('/get_link')
 def get_links():
     for link in links_spit():
@@ -25,39 +21,6 @@ def get_links():
 def get_links():
     links.append(links_spit())
     return jsonify({'links': links})
-    
-
-'''@app.route('/get_link')
-def get_links():
-    links = []
-    for link in links_spit():
-        links.append(link)
-    return jsonify({'links': links})'''
-    
-'''@app.route('/get_link')
-def get_links():
-    def generate_links():
-        for link in links_spit():
-            yield link
-            links.append(link)
-    
-    return Response(generate_links(), mimetype='text/event-stream')'''
-
-
-'''@app.route('/get_link')
-def get_links():
-    # Use a generator expression to yield links one by one
-    links_gen = links_spit()
-    for i in range(len(links)):
-        try:
-            # Try to get the next link from the generator expression
-            link = next(links_gen)
-            links.append(link)
-        except StopIteration:
-            # If there are no more links to yield, break out of the loop
-            break
-    return jsonify({'links': links})'''
-
 
 
 
